2398a75125df278a3a7596cb794beeed205ef5b4
HardyJL <j.hardenberg@hotmail.de>
2025-08-05 16:28:17 +0200

    dev commit

--- init.lua
+++ init.lua
@@ -1,6 +1,44 @@
-require("config.lazy")
-require("config.options")
-require("config.keymaps")
-require("config.autocmd")
-require("config.terminal")
-vim.cmd.colorscheme("hybrid")
+vim.opt.winborder = "rounded"
+vim.opt.tabstop = 2
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.signcolumn = "yes"
+
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
+map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+
+vim.pack.add({
+	{ src = "https://github.com/HoNamDuong/hybrid.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+})
+
+require "oil".setup()
+
+map('n', '<leader>e', ":Oil<CR>")
+map('t', '', "")
+map('t', '', "")
+map('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.lsp.enable({ "lua_ls" })
+
+-- colors
+require "hybrid".setup({ transparent = true })
+vim.cmd("colorscheme hybrid")
+vim.cmd(":hi statusline guibg=NONE")
